5f3cf8ace6b487914292c2c9b9fb4d893b7e3f13
yuchen <smirkiiiv@gmail.com>
2020-05-11 23:09:23 +0800

    Initial Commit

---
+++ segmentControllAdd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  segmentControllAdd
+//
+//  Created by 林祐辰 on 2020/5/11.
+//  Copyright © 2020 smirkiiiv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
