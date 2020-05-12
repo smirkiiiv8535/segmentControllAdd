b6178110d806cf7f3adb50bbe46b7678f5f34377
yuchen <smirkiiiv@gmail.com>
2020-05-12 17:53:08 +0800

    update

--- segmentControllAdd/ViewController.swift
+++ segmentControllAdd/ViewController.swift
@@ -14,8 +14,11 @@ class ViewController: UIViewController {
     @IBOutlet weak var secondNum: UITextField!
     @IBOutlet weak var calculateSegment: UISegmentedControl!
     @IBOutlet weak var resultNum: UILabel!
+    
     var biggerTimer = Timer()
     var smallerTimer = Timer()
+    
+    // 預設字體大小
     var defaultSize = 50
 
     
@@ -26,55 +29,69 @@ class ViewController: UIViewController {
 
     @IBAction func calculateHelper(_ sender: UISegmentedControl) {
         
+        // 將計算後的數值存入此變數裡
         var sumNumber :Double? = 0
+        
+        // 將文字輸入框的字串轉換為浮點數
         let firstNumber = Double(firstNum.text!)
         let secondNumber = Double(secondNum.text!)
         
-    if (firstNumber != nil && secondNumber != nil){
         
-          if (sender.selectedSegmentIndex == 0){
-           sumNumber = firstNumber! + secondNumber!
+      // 如果文字輸入框有輸入數字才做計算, 不然就會跳出提示訊息
+        
+        if (firstNumber != nil && secondNumber != nil){
+        
+           if (sender.selectedSegmentIndex == 0){
+              sumNumber = firstNumber! + secondNumber!
+              numberTurn()
           }else if(sender.selectedSegmentIndex == 1){
             sumNumber = firstNumber! - secondNumber!
+            numberTurn()
           }else if(sender.selectedSegmentIndex == 2){
             sumNumber = firstNumber! * secondNumber!
+            numberTurn()
            }else{
             sumNumber = firstNumber! / secondNumber!
-             resultNum.text = String(format: "%.2f", sumNumber!)
+            numberTurn()
             }
-        }else{
+      }else{
             let alertView = UIAlertController(title: "未輸入數字", message: "請在文字框輸入數字", preferredStyle: .alert)
             let alertViewButton = UIAlertAction(title: "OK", style: .default, handler: nil)
             alertView.addAction(alertViewButton)
             present(alertView,animated: true,completion: nil)
-            resultNum.text = ""
+            biggerTimer.invalidate()
+            smallerTimer.invalidate()
         }
     resultNum.text = String(format: "%.2f", sumNumber!)
-    numberTurn()
     renderSmallerNum ()
     renderBiggerNum ()
-  }
-
-     func numberTurn(){
     
-     let turnBack = CAKeyframeAnimation(keyPath: "transform.rotation")
+  }
+    
+    
+// 透過 KeyframeAnimation 實現字體 順時針/逆時針 旋轉
+     func numberTurn(){
+      let turnBack = CAKeyframeAnimation(keyPath: "transform.rotation")
          turnBack.values = [CGFloat.pi*2,CGFloat.pi,0,0,CGFloat.pi,CGFloat.pi*2]
          turnBack.keyTimes = [0,NSNumber(value: 0.2),NSNumber(value:0.4),NSNumber(value: 0.6),NSNumber(value: 0.8),NSNumber(value: 1.0)]
-         turnBack.duration = 8
+         turnBack.duration = 12
          turnBack.repeatCount = 0
          turnBack.fillMode = CAMediaTimingFillMode.forwards
+        
          resultNum.layer.add(turnBack,forKey: nil)
      }
-    
 
+    
+    // 設定將字體放大的計時器
     func renderBiggerNum (){
-        biggerTimer = Timer.scheduledTimer(withTimeInterval: 10, repeats: true, block: { (biggerTimer) in
+        biggerTimer = Timer.scheduledTimer(withTimeInterval: 7, repeats: true, block: { (biggerTimer) in
             let sizeExchanger :CGFloat = (CGFloat(self.defaultSize)+CGFloat(20))
             let font = self.resultNum.font
             self.resultNum.font = font?.withSize(sizeExchanger)
         })
     }
     
+    // 設定將字體縮小的計時器
     func renderSmallerNum (){
         smallerTimer = Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { (smallerTimer) in
             let sizeExchanger :CGFloat = (CGFloat(self.defaultSize)-CGFloat(5))
